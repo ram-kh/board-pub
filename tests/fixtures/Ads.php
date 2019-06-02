cd7b93293e1c0e42c150ce6f4dae50c72dd7bdf3
Ramil Khuzin <ram_kh@mail.ru>
2019-06-02 22:22:21 +0500

    Project "board"
    Signed-off-by: Ramil Khuzin <ram_kh@mail.ru>

---
+++ tests/fixtures/Ads.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Рамиль
+ * Date: 13.04.2019
+ * Time: 17:28
+ */
+
+namespace app\tests\fixtures;
+
+use yii\test\ActiveFixture;
+
+class Ads extends ActiveFixture
+{
+    public $modelClass = 'app\models\Ads';
+
+}
